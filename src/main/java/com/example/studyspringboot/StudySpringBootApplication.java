187ce804fc570f030c6323491e043f6b8b3594fb
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-08 21:21:17 +0900

    ⚙ :: Setting

---
+++ src/main/java/com/example/studyspringboot/StudySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootApplication.class, args);
+    }
+
+}
