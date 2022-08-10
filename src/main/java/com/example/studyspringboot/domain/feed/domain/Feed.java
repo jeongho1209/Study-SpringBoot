a513bf9aaafb2e9d24d248794735345225701790
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-10 23:37:30 +0900

    ⚡ :: BaseTimeEntity 상속 추가

--- src/main/java/com/example/studyspringboot/domain/feed/domain/Feed.java
+++ src/main/java/com/example/studyspringboot/domain/feed/domain/Feed.java
@@ -1,19 +1,22 @@
 package com.example.studyspringboot.domain.feed.domain;
 
 import com.example.studyspringboot.domain.user.domain.User;
+import com.example.studyspringboot.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Feed {
+public class Feed extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,14 +30,19 @@ public class Feed {
     @Size(max = 500)
     private String content;
 
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime updatedAt;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Builder
-    public Feed(String title, String content, User user) {
+    public Feed(String title, String content, LocalDateTime updatedAt, User user) {
         this.title = title;
         this.content = content;
+        this.updatedAt = updatedAt;
         this.user = user;
     }
 
