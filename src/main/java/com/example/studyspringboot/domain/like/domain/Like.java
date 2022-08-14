3c730a9ade6ce996674375dd3b38ac2002f3c8b4
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-15 00:21:12 +0900

    ⚡ :: Like 엔티티 추가

---
+++ src/main/java/com/example/studyspringboot/domain/like/domain/Like.java
@@ -0,0 +1,36 @@
+package com.example.studyspringboot.domain.like.domain;
+
+import com.example.studyspringboot.domain.feed.domain.Feed;
+import com.example.studyspringboot.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Like(Feed feed, User user) {
+        this.feed = feed;
+        this.user = user;
+    }
+
+
+}
