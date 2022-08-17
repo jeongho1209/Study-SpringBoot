15e4c69666d520f7efc96b44ce559ff2a52f72ff
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-17 23:53:53 +0900

    📑 :: JwtProperty

---
+++ src/main/java/com/example/studyspringboot/global/property/EnableConfigurationPropertiesConfig.java
@@ -0,0 +1,10 @@
+package com.example.studyspringboot.global.property;
+
+import com.example.studyspringboot.global.security.jwt.JwtProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigurationProperties(value = JwtProperty.class)
+@Configuration
+public class EnableConfigurationPropertiesConfig {
+}
