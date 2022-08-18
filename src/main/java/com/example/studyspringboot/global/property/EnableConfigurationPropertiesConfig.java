cdc339b933fe3cbf4aa6c37a8337efd14e532388
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-18 22:12:23 +0900

    ♻ :: {} 추가

--- src/main/java/com/example/studyspringboot/global/property/EnableConfigurationPropertiesConfig.java
+++ src/main/java/com/example/studyspringboot/global/property/EnableConfigurationPropertiesConfig.java
@@ -4,7 +4,7 @@ import com.example.studyspringboot.global.security.jwt.JwtProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties(value = JwtProperty.class)
+@EnableConfigurationProperties(value = {JwtProperty.class})
 @Configuration
 public class EnableConfigurationPropertiesConfig {
 }
