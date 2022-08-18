41719b58af1380e601a08d5e9acc0a87d9368ebe
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-18 22:48:49 +0900

    ✔ :: JavaMailSender 빈 따로 등록

---
+++ src/main/java/com/example/studyspringboot/global/email/EmailConfig.java
@@ -0,0 +1,16 @@
+package com.example.studyspringboot.global.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+
+}
