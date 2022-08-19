4447e1e0c3797a5f0d2a79345652bb1ccf14b2c4
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-19 17:50:31 +0900

    ⚙ :: project Setting

---
+++ CategoryBoard/src/main/java/com/example/categoryboard/CategoryBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.categoryboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryBoardApplication.class, args);
+    }
+
+}
---
+++ CategoryBoard/src/test/java/com/example/categoryboard/CategoryBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.categoryboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
