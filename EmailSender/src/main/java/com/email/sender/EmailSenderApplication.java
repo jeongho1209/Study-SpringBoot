6b4fb543eb705cb39c51b6cf95c7f7ba431df893
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-21 15:03:36 +0900

    ⚙ :: setting

---
+++ EmailSender/src/main/java/com/email/sender/EmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.email.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSenderApplication.class, args);
+    }
+
+}
