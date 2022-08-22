0e50d54558bfa4c1a45ca921d4996013d63eda7d
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-22 16:24:03 +0900

    📑 :: UserFacade

---
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/user/facade/UserFacade.java
@@ -0,0 +1,22 @@
+package com.example.studyspringboot.domain.user.facade;
+
+import com.example.studyspringboot.domain.user.domain.User;
+import com.example.studyspringboot.domain.user.domain.repository.UserRepository;
+import com.example.studyspringboot.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
