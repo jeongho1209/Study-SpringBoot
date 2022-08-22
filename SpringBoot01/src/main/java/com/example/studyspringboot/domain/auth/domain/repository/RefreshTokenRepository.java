a4106d3103f5222b27d3a915d5d985c15dcb90b9
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-22 16:07:16 +0900

    📑 :: @Repository 어노테이션

--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/domain/repository/RefreshTokenRepository.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -2,6 +2,8 @@ package com.example.studyspringboot.domain.auth.domain.repository;
 
 import com.example.studyspringboot.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
