4bb02505801c9250969f4525245079cb77e90df0
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-26 19:03:19 +0900

    ⚙ :: 프로젝트 세팅

---
+++ OAuth/src/main/java/com/example/oauth/OAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthApplication.class, args);
+    }
+
+}
