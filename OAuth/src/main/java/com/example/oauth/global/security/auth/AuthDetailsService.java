56c2a533bc7e2196351f329c190c38f9102139e1
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-27 09:05:24 +0900

    📑 :: AuthDetails

---
+++ OAuth/src/main/java/com/example/oauth/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.oauth.global.security.auth;
+
+import com.example.oauth.domain.user.domain.repository.UserRepository;
+import com.example.oauth.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
