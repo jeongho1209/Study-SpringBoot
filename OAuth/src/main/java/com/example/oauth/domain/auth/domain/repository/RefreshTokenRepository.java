8419fc830efcd00e1cbf68c3c5881a5dd66e014b
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-27 09:03:38 +0900

    📑 :: RefreshToken

---
+++ OAuth/src/main/java/com/example/oauth/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.example.oauth.domain.auth.domain.repository;
+
+import com.example.oauth.domain.auth.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String refreshToken);
+
+}
