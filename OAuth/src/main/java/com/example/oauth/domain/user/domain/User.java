3037aa296bb9ef64988dbdcaeeb291cd8f647ab0
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-05 08:09:43 +0900

    ⚡ :: User 엔티티 추가

--- OAuth/src/main/java/com/example/oauth/domain/user/domain/User.java
+++ OAuth/src/main/java/com/example/oauth/domain/user/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.oauth.domain.user.domain;
 
+import com.example.oauth.domain.user.domain.type.ProviderType;
+import com.example.oauth.domain.user.domain.type.Role;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,10 +24,28 @@ public class User {
     @Column(length = 10, nullable = false)
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProviderType providerType;
+
     @Builder
-    public User(String email, String name) {
+    public User(String email, String name, ProviderType providerType, Role role) {
         this.email = email;
         this.name = name;
+        this.providerType = providerType;
+        this.role = role;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return this.role.getRole();
     }
 
 }
