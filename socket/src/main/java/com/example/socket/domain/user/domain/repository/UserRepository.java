6ebbd7adeae81c90d085901f2ba742b06bb6664f
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-07 14:16:02 +0900

    📑 :: UserRepository

---
+++ socket/src/main/java/com/example/socket/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.socket.domain.user.domain.repository;
+
+import com.example.socket.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
