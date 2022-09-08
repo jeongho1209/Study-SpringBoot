0e6bfe47ec3d98dc13fb0472f4b97cd070d3ab18
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-08 21:55:56 +0900

    ✅ :: ResponseStatus 추가

--- socket/src/main/java/com/example/socket/domain/user/presentation/UserController.java
+++ socket/src/main/java/com/example/socket/domain/user/presentation/UserController.java
@@ -4,10 +4,8 @@ import com.example.socket.domain.auth.presentation.dto.response.TokenResponse;
 import com.example.socket.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.socket.domain.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,6 +16,7 @@ public class UserController {
 
     private final SignUpService signUpService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public TokenResponse signUp(@RequestBody @Valid SignUpRequest request) {
         return signUpService.execute(request);
