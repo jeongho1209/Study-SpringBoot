264e5f3bd06e3cf10ae234c254d3c8f515b0a10b
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-10 07:43:35 +0900

    ⚡ :: 토큰 재발급 api

--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/domain/repository/RefreshTokenRepository.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -4,6 +4,11 @@ import com.example.studyspringboot.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String refreshToken);
+
 }
---
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/service/ReissueService.java
@@ -0,0 +1,43 @@
+package com.example.studyspringboot.domain.auth.service;
+
+import com.example.studyspringboot.domain.auth.domain.RefreshToken;
+import com.example.studyspringboot.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.studyspringboot.domain.auth.exception.InvalidRefreshTokenException;
+import com.example.studyspringboot.domain.auth.exception.RefreshTokenNotFoundException;
+import com.example.studyspringboot.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.studyspringboot.global.security.jwt.JwtProperty;
+import com.example.studyspringboot.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ReissueService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtProperty jwtProperty;
+
+    @Transactional
+    public TokenResponse reissue(String refreshToken) {
+        String parseToken = jwtTokenProvider.parseToken(refreshToken);
+        if (parseToken == null) {
+            throw InvalidRefreshTokenException.EXCEPTION;
+        }
+
+        RefreshToken redisRefreshToken = refreshTokenRepository.findByToken(jwtTokenProvider.parseToken(refreshToken))
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+
+        String newRefreshToken = jwtTokenProvider.generateRefreshToken(redisRefreshToken.getAccountId());
+        redisRefreshToken.updateToken(newRefreshToken, jwtProperty.getRefreshExp());
+
+        String newAccessToken = jwtTokenProvider.generateAccessToken(redisRefreshToken.getAccountId());
+
+        return TokenResponse.builder()
+                .accessToken(newAccessToken)
+                .refreshToken(newRefreshToken)
+                .build();
+    }
+
+}
