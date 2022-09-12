281900cc3808cd8cc49627256ce7609df8955079
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-12 12:46:51 +0900

    📑 :: BaseTimeEntity

---
+++ socket/src/main/java/com/example/socket/global/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.example.socket.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @NotNull
+    private LocalDateTime sendAt;
+
+}
