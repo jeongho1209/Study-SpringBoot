027ea090952826fbbf1e98bb115babac30d4c9b6
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-12 23:27:31 +0900

    ⚡ :: 토큰 재발급 api

--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/presentation/AuthController.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/presentation/AuthController.java
@@ -2,13 +2,11 @@ package com.example.studyspringboot.domain.auth.presentation;
 
 import com.example.studyspringboot.domain.auth.presentation.dto.request.UserSignInRequest;
 import com.example.studyspringboot.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.studyspringboot.domain.auth.service.ReissueService;
 import com.example.studyspringboot.domain.auth.service.TokenService;
 import com.example.studyspringboot.global.enums.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,10 +16,16 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final TokenService tokenService;
+    private final ReissueService reissueService;
 
     @PostMapping("/token")
     public TokenResponse signIn(@RequestBody @Valid UserSignInRequest request, Role role) {
         return tokenService.signIn(request, role);
+    }
+
+    @PatchMapping("/token")
+    public TokenResponse reissue(String refreshToken, Role role) {
+        return reissueService.reissue(refreshToken, role);
     }
 
 }
--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/service/ReissueService.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/auth/service/ReissueService.java
@@ -5,6 +5,7 @@ import com.example.studyspringboot.domain.auth.domain.repository.RefreshTokenRep
 import com.example.studyspringboot.domain.auth.exception.InvalidRefreshTokenException;
 import com.example.studyspringboot.domain.auth.exception.RefreshTokenNotFoundException;
 import com.example.studyspringboot.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.studyspringboot.global.enums.Role;
 import com.example.studyspringboot.global.security.jwt.JwtProperty;
 import com.example.studyspringboot.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class ReissueService {
     private final JwtProperty jwtProperty;
 
     @Transactional
-    public TokenResponse reissue(String refreshToken) {
+    public TokenResponse reissue(String refreshToken, Role role) {
         String parseToken = jwtTokenProvider.parseToken(refreshToken);
         if (parseToken == null) {
             throw InvalidRefreshTokenException.EXCEPTION;
@@ -29,10 +30,10 @@ public class ReissueService {
         RefreshToken redisRefreshToken = refreshTokenRepository.findByToken(jwtTokenProvider.parseToken(refreshToken))
                 .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
 
-        String newRefreshToken = jwtTokenProvider.generateRefreshToken(redisRefreshToken.getAccountId());
+        String newRefreshToken = jwtTokenProvider.generateRefreshToken(redisRefreshToken.getAccountId(), role);
         redisRefreshToken.updateToken(newRefreshToken, jwtProperty.getRefreshExp());
 
-        String newAccessToken = jwtTokenProvider.generateAccessToken(redisRefreshToken.getAccountId());
+        String newAccessToken = jwtTokenProvider.generateAccessToken(redisRefreshToken.getAccountId(), role);
 
         return TokenResponse.builder()
                 .accessToken(newAccessToken)
