abeef5d55da1146b8ab2b418402061f048c6383c
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-23 20:29:16 +0900

    ⚡ :: Role 추가

---
+++ OAuth/src/main/java/com/example/oauth/domain/user/domain/type/Role.java
@@ -0,0 +1,14 @@
+package com.example.oauth.domain.user.domain.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER");
+
+    private final String role;
+
+}
