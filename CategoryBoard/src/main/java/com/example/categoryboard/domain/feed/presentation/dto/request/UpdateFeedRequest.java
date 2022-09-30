8e6811b1c005546c459c69d2ec81275851e22a04
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-30 21:33:54 +0900

    ♻ :: Validation 메세지 삭제

--- CategoryBoard/src/main/java/com/example/categoryboard/domain/feed/presentation/dto/request/UpdateFeedRequest.java
+++ CategoryBoard/src/main/java/com/example/categoryboard/domain/feed/presentation/dto/request/UpdateFeedRequest.java
@@ -3,19 +3,19 @@ package com.example.categoryboard.domain.feed.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class UpdateFeedRequest {
 
-    @NotNull(message = "title은 Null이 불가합니다.")
-    @Size(max = 20, message = "title은 최대 20자입니다.")
+    @NotBlank
+    @Size(max = 20)
     private String title;
 
-    @NotNull(message = "content는 Null이 불가합니다.")
-    @Size(max = 500, message = "content는 최대 500자입니다.")
+    @NotBlank
+    @Size(max = 500)
     private String content;
 
 }
