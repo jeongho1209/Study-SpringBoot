e9f8952407e72519c07190da65b9942bd29e51ea
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-10-04 08:14:32 +0900

    📑 :: RoomType Enum 추가

---
+++ socket/src/main/java/com/example/socket/domain/chat/domain/enums/RoomType.java
@@ -0,0 +1,13 @@
+package com.example.socket.domain.chat.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RoomType {
+
+    APPLY,
+    INQUIRY
+
+}
