da0690e8c3b44415ab04d430bc8c3a55c9baf76d
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-10-17 21:45:53 +0900

    ♻️ :: 코드 정렬

--- socket/src/main/java/com/example/socket/domain/chat/domain/repository/RoomRepository.java
+++ socket/src/main/java/com/example/socket/domain/chat/domain/repository/RoomRepository.java
@@ -4,4 +4,5 @@ import com.example.socket.domain.chat.domain.Room;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoomRepository extends CrudRepository<Room, Integer> {
+
 }
