eb330dfa1920d6c637b834e97ec4c079a9a9849c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-10-18 17:27:56 +0900

    ♻️ :: import 정리

--- socket/src/main/java/com/example/socket/domain/chat/presentation/ChatSocketController.java
+++ socket/src/main/java/com/example/socket/domain/chat/presentation/ChatSocketController.java
@@ -13,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RequiredArgsConstructor
 @RestController
 public class ChatSocketController {
