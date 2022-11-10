ccbe3351579c3aa80b7b0f74d958b39c5c41ae7c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-10 23:32:41 +0900

    ⚡️ :: 로그인 api

--- fcm/src/main/java/com/example/fcm/domain/user/presentation/UserController.java
+++ fcm/src/main/java/com/example/fcm/domain/user/presentation/UserController.java
@@ -1,6 +1,9 @@
 package com.example.fcm.domain.user.presentation;
 
-import com.example.fcm.domain.user.presentation.dto.UserSignUpRequest;
+import com.example.fcm.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.fcm.domain.user.presentation.dto.request.UserSignInRequest;
+import com.example.fcm.domain.user.presentation.dto.request.UserSignUpRequest;
+import com.example.fcm.domain.user.service.UserSignInService;
 import com.example.fcm.domain.user.service.UserSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +17,17 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserSignUpService userSignUpService;
+    private final UserSignInService userSignInService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void signUp(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.execute(request);
+    }
+
+    @PostMapping("/token")
+    public TokenResponse signIn(@RequestBody @Valid UserSignInRequest request) {
+        return userSignInService.execute(request);
     }
 
 }
---
+++ fcm/src/main/java/com/example/fcm/domain/user/service/UserSignInService.java
@@ -0,0 +1,41 @@
+package com.example.fcm.domain.user.service;
+
+import com.example.fcm.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.fcm.domain.user.domain.User;
+import com.example.fcm.domain.user.domain.UserRepository;
+import com.example.fcm.domain.user.exception.PasswordMisMatchException;
+import com.example.fcm.domain.user.presentation.dto.request.UserSignInRequest;
+import com.example.fcm.global.exception.UserNotFoundException;
+import com.example.fcm.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserSignInService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Transactional
+    public TokenResponse execute(UserSignInRequest request) {
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw PasswordMisMatchException.EXCEPTION;
+        }
+
+        String accessToken = jwtTokenProvider.generateAccessToken(request.getAccountId());
+        String refreshToken = jwtTokenProvider.generateRefreshToken(request.getAccountId());
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+}
