703b6c77b9de3e28b9a86e8bac07147aef712102
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-10 23:32:55 +0900

    ⚡️ :: @EnableJpaAuditing

--- fcm/src/main/java/com/example/fcm/FcmApplication.java
+++ fcm/src/main/java/com/example/fcm/FcmApplication.java
@@ -3,7 +3,9 @@ package com.example.fcm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FcmApplication {
