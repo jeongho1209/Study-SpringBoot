b8c7d29acd002e0cda97205777a46e09fc2d0e35
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-10 23:32:02 +0900

    📑 :: Room

---
+++ fcm/src/main/java/com/example/fcm/domain/chat/domain/room/Room.java
@@ -0,0 +1,28 @@
+package com.example.fcm.domain.chat.domain.room;
+
+import com.example.fcm.domain.chat.domain.roomuser.RoomUser;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String roomName;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<RoomUser> roomUsers = new ArrayList<>();
+
+}
---
+++ fcm/src/main/java/com/example/fcm/domain/chat/domain/room/RoomRepository.java
@@ -0,0 +1,6 @@
+package com.example.fcm.domain.chat.domain.room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
