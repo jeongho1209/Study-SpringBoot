7d49ddacefe8327b9d62ac4a5882eef504daf79f
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-11 23:41:42 +0900

    📑 :: UserFacade

--- fcm/src/main/java/com/example/fcm/domain/user/domain/User.java
+++ fcm/src/main/java/com/example/fcm/domain/user/domain/User.java
@@ -6,15 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "tbl_user")
 public class User {
 
     @Id
---
+++ fcm/src/main/java/com/example/fcm/domain/user/facade/UserFacade.java
@@ -0,0 +1,30 @@
+package com.example.fcm.domain.user.facade;
+
+import com.example.fcm.domain.user.domain.User;
+import com.example.fcm.domain.user.domain.UserRepository;
+import com.example.fcm.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getAccountId(accountId);
+    }
+
+    public User findUserByAccountId(String accountId) {
+        return getAccountId(accountId);
+     }
+
+     public User getAccountId(String accountId) {
+        return userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+     }
+
+}
