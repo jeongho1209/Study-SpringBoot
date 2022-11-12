a016c4def118dc7d25d766f928bfdc1f032e630a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-12 18:03:57 +0900

    ⚡️ :: 회원 탈퇴 api

---
+++ fcm/src/main/java/com/example/fcm/domain/user/service/UserWithdrawalService.java
@@ -0,0 +1,24 @@
+package com.example.fcm.domain.user.service;
+
+import com.example.fcm.domain.user.domain.User;
+import com.example.fcm.domain.user.domain.UserRepository;
+import com.example.fcm.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserWithdrawalService {
+
+    private final UserRepository userRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute() {
+        User user = userFacade.getCurrentUser();
+
+        userRepository.delete(user);
+    }
+
+}
