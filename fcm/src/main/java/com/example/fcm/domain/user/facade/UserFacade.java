37af86c7d84d8ba7586c395a3066cd5f7b5027b0
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-13 16:50:19 +0900

    ♻️ :: 메소드 수정

--- fcm/src/main/java/com/example/fcm/domain/user/facade/UserFacade.java
+++ fcm/src/main/java/com/example/fcm/domain/user/facade/UserFacade.java
@@ -1,10 +1,8 @@
 package com.example.fcm.domain.user.facade;
 
-import com.corundumstudio.socketio.SocketIOClient;
 import com.example.fcm.domain.user.domain.User;
 import com.example.fcm.domain.user.domain.UserRepository;
 import com.example.fcm.global.exception.UserNotFoundException;
-import com.example.fcm.global.websocket.property.ClientProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -28,10 +26,4 @@ public class UserFacade {
         return userRepository.findByAccountId(accountId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
-
-    public User findUserByClient(SocketIOClient socketIOClient) {
-        return userRepository.findById(socketIOClient.get(ClientProperty.USER_KEY))
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-    }
-
 }
