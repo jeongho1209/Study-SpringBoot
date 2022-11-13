e6fe1bfaf466c421c80bf143e85056866c45c8b7
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-14 08:28:25 +0900

    ⚡️ :: 쿼리 추가

--- fcm/src/main/java/com/example/fcm/domain/chat/domain/room/RoomRepository.java
+++ fcm/src/main/java/com/example/fcm/domain/chat/domain/room/RoomRepository.java
@@ -2,5 +2,9 @@ package com.example.fcm.domain.chat.domain.room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    List<Room> findRoomsByOrderByIdDesc();
 }
--- fcm/src/main/java/com/example/fcm/domain/chat/domain/roomuser/RoomUserRepository.java
+++ fcm/src/main/java/com/example/fcm/domain/chat/domain/roomuser/RoomUserRepository.java
@@ -1,6 +1,15 @@
 package com.example.fcm.domain.chat.domain.roomuser;
 
+import com.example.fcm.domain.chat.domain.room.Room;
+import com.example.fcm.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomUserRepository extends JpaRepository<RoomUser, Long> {
+
+    Optional<RoomUser> findByUserAndRoom(User user, Room room);
+
+    boolean existsByUserAndRoom(User user, Room room);
+
 }
