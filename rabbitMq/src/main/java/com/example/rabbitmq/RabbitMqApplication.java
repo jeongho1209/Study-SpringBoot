8d7cb27d14025eec26b16017780fbd9e8d2f9997
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-28 15:18:55 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ rabbitMq/src/main/java/com/example/rabbitmq/RabbitMqApplication.java
@@ -0,0 +1,21 @@
+package com.example.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqApplication.class, args);
+    }
+
+}
