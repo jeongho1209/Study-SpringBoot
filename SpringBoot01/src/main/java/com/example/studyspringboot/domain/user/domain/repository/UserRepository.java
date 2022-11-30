23f0b74a3398aabbebe33c1ac74c1bd346ae0b49
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-30 13:57:01 +0900

    ⚡️ :: 쿼리 추가

--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/user/domain/repository/UserRepository.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/user/domain/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.studyspringboot.domain.user.domain.repository;
 
 import com.example.studyspringboot.domain.user.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
@@ -10,5 +12,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByAccountId(String accountId);
 
     Optional<User> findByEmail(String email);
+
+    List<User> queryUserByFollowCountsLessThan(Integer key);
 
 }
