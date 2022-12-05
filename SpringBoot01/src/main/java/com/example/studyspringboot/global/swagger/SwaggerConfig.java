16c95e6b6d8cb2a6ab5746d0c790bb105f56a387
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-05 23:37:20 +0900

    📑 :: SwaggerConfig 추가

---
+++ SpringBoot01/src/main/java/com/example/studyspringboot/global/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.studyspringboot.global.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("테스트")
+                        .description("프로젝트 API 명세서입니다.")
+                        .version("v1"));
+    }
+
+}
