0b89028d0b062eb0680142d79e86be3dd13dac37
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-06 22:38:58 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ clone/src/main/kotlin/com/example/clone/CloneApplication.kt
@@ -0,0 +1,14 @@
+package com.example.clone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloneApplication>(*args)
+}
+
+
+
