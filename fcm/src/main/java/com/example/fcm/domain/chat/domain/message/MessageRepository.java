a030f642059aba6fda7c95ddc68b9c3d538a058a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-19 09:09:41 +0900

    feat :: 메소드

--- fcm/src/main/java/com/example/fcm/domain/chat/domain/message/MessageRepository.java
+++ fcm/src/main/java/com/example/fcm/domain/chat/domain/message/MessageRepository.java
@@ -2,5 +2,11 @@ package com.example.fcm.domain.chat.domain.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findAllBy();
+
+
 }
