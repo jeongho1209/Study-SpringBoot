ba07d9b045134ceedad7f22099e8936fabd84a79
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-21 13:01:52 +0900

    ♻️ :: url 수정

--- SpringBoot01/src/main/java/com/example/studyspringboot/global/swagger/SwaggerConfig.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/global/swagger/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("v1-definition")
-                .pathsToMatch("/api/**")
+                .pathsToMatch("/**")
                 .build();
     }
 
