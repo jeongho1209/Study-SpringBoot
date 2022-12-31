ac9f2ce56042b089c7d25f336aafa481fd961af2
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-31 23:41:15 +0900

    ⚙️ :: 세팅

---
+++ jpql/src/test/java/com/example/jpql/JpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
