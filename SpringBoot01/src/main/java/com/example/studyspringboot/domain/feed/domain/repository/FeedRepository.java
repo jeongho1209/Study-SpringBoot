8bf6751e6b89de68d7f313e5e6dc85462ae5d765
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-04 22:31:08 +0900

    ⚡️ :: 쿼리 추가

--- SpringBoot01/src/main/java/com/example/studyspringboot/domain/feed/domain/repository/FeedRepository.java
+++ SpringBoot01/src/main/java/com/example/studyspringboot/domain/feed/domain/repository/FeedRepository.java
@@ -1,18 +1,24 @@
 package com.example.studyspringboot.domain.feed.domain.repository;
 
 import com.example.studyspringboot.domain.feed.domain.Feed;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface FeedRepository extends CrudRepository<Feed, Integer> {
+public interface FeedRepository extends CrudRepository<Feed, Integer>, FeedRepositoryCustom {
 
     @Query("select f from Feed f join fetch f.user order by f.createdAt DESC ")
     List<Feed> findAllByJoinFetch();
 
     List<Feed> findAllByTitleContaining(String title);
+
+    @Query("select f from Feed f where f.createdAt =: created_at")
+    List<Feed> queryTest(LocalDate createdAt);
 
 }
