6d380767e2a7f2dbeee5067438206836048a902e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-10 09:42:47 +0900

    ⚡️ :: 예제

---
+++ spring-batch/src/main/java/com/example/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("==================");
+                    System.out.println("Hello Spring Batch");
+                    System.out.println("==================");
+
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
