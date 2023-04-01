64e5b2db8f09d171f60ad5462bd10366d8518105
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-01 13:02:32 +0900

    ♻️ :: 삭제 확인 로직 수정

--- fcm/src/main/java/com/example/fcm/domain/chat/service/DeleteRoomService.java
+++ fcm/src/main/java/com/example/fcm/domain/chat/service/DeleteRoomService.java
@@ -1,6 +1,8 @@
 package com.example.fcm.domain.chat.service;
 
 import com.example.fcm.domain.chat.domain.room.Room;
+import com.example.fcm.domain.chat.domain.room.RoomRepository;
+import com.example.fcm.domain.chat.exception.CannotDeleteRoomException;
 import com.example.fcm.domain.chat.facade.RoomFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class DeleteRoomService {
 
+    private final RoomRepository roomRepository;
     private final RoomFacade roomFacade;
 
     @Transactional
     public void execute(Long roomId) {
         Room room = roomFacade.getRoom(roomId);
 
-        roomFacade.checkRoom(room);
-    }
+        boolean isCheckExistRoomUsers = room.getRoomUsers().size() != 0;
 
+        if (isCheckExistRoomUsers) {
+            throw CannotDeleteRoomException.EXCEPTION;
+        }
+
+        roomRepository.delete(room);
+    }
 }
